d888158b6b8cff6c36476ef3191763608ed1da24
vxm52 <vaibhav.s.malla@gmail.com>
2020-08-16 14:42:08 -0400

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+
+import NewsCards from './components/NewsCards/NewsCards';
+
+const alanKey = '';
+
+const App = () => {
+    const [newsArticles, setNewsArticles] = useState([]);
+    
+    useEffect(() => {
+        alanBtn({
+            key: alanKey,  
+            onCommand: ({ command, articles }) => {
+                if(command === 'newHeadlines') {
+                    setNewsArticles(articles);
+                }
+            }
+        })
+    }, [])
+    
+    return (
+        <div>
+            <h1>Alan AI News Application</h1>
+            <NewsCards articles={newsArticles} />
+        </div>
+    )
+}
+
+export default App;
